2a47f1fac6d95bb159e4dbd082cdd9358f66a915
Alesya Makarevich <122222016+AlesyaMakarevich@users.noreply.github.com>
2023-01-25 23:08:38 +0300

    First commit

---
+++ hw4/task1.php
@@ -0,0 +1,49 @@
+<?php
+
+
+$departments = [
+    'developers' => [
+        [
+            'name' => 'nik',
+            'position' => 'developer',
+            'salary' => 900
+        ],
+        [
+            'name' => 'sergey',
+            'position' => 'developer',
+            'salary' => 1200
+        ],
+        [
+            'name' => 'andrey',
+            'position' => 'developer',
+            'salary' => 1400
+        ]
+    ],
+    'managers' => [
+        [
+            'name' => 'yulia',
+            'position' => 'manager',
+            'salary' => 1150
+        ],
+        [
+            'name' => 'darya',
+            'position' => 'manager',
+            'salary' => 1100
+        ],
+        [
+            'name' => 'viktoria',
+            'position' => 'manager',
+            'salary' => 1000
+        ]
+    ],
+];
+$averageSalaries = [];
+
+foreach ($departments as $nameDepartments => $descriptions) {
+    $averageSalaries[$nameDepartments] = array_sum(array_map(function ($sum) {
+            return $sum['salary'];
+        }, $descriptions)) / count($descriptions);
+}
+
+print_r($averageSalaries);
+
---
+++ hw4/task3.php
@@ -0,0 +1,32 @@
+<?php
+
+$users = [
+    [
+        'phone' => '375295675435',
+        'name' => 'Kirill',
+        'password' => '#`7q+3}VCz-&R&.y',
+        'updated_password_at' => '10.08.2022'
+    ],
+    [
+        'phone' => '375334564356',
+        'name' => 'Max',
+        'password' => '%n#wSEdL~6XDKmGf',
+        'updated_password_at' => '15.01.2023'
+    ],
+    [
+        'phone' => '48295675435',
+        'name' => 'Andrey',
+        'password' => '%NYD9Bu%Y43"vsS*',
+        'updated_password_at' => '11.12.2022'
+    ]
+];
+
+foreach ($users as &$user) {
+    $diffUpdatedPass = time() - strtotime($user['updated_password_at']);
+    $diffUpdatedPassInDay = $diffUpdatedPass / (60 * 60 * 24);
+    if ($diffUpdatedPassInDay > 90) {
+        $user['password'] = 'new_password';
+    }
+}
+
+print_r($users);
